1f7602f4ff13e45d094c6532bed1157f7f746413
PSKutergin <PSKutergin@gmail.com>
2024-07-01 08:11:15 +0300

    added pages and common components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/pages/main/main.component';
+import { ProductsComponent } from './components/pages/products/products.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { FooterComponent } from './components/common/footer/footer.component';
+import { ProductComponent } from './components/pages/product/product.component';
+import { OrderComponent } from './components/pages/order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ProductsComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
